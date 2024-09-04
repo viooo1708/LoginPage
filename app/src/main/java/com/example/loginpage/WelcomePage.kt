ef26e2b75a38d1bc0752f23bf04c59ced275ce23
viooo1708 <vionamahardikasafitri1784@gmail.com>
2024-09-04 20:01:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/loginpage/WelcomePage.kt
@@ -0,0 +1,30 @@
+package com.example.loginpage
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class WelcomePage : AppCompatActivity() {
+    private lateinit var txtHasilnya : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_welcome_page)
+
+        txtHasilnya = findViewById(R.id.txtWelcome)
+
+        val getUsername = intent.getStringExtra("username")
+        val getPassword = intent.getStringExtra("password")
+        txtHasilnya.setText("Hello ${getUsername}, Password anda adalah ${getPassword}")
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
